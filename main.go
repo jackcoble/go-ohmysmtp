c4bd473ef1b8964ce902a0773f52aa9efd0232ba
Jack Coble <jack@coble.uk>
2021-12-13 19:36:27 +0000

    Basic API client

---
+++ main.go
@@ -0,0 +1,61 @@
+package ohmysmtp
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Production endpoint for OhMySMTP
+const API_URL = "https://app.ohmysmtp.com/api/v1"
+
+type Client struct {
+	apiKey string
+}
+
+// Expected payload when sending to API
+type Payload struct {
+	From     string `json:"from"`
+	To       string `json:"to"`
+	Subject  string `json:"subject"`
+	TextBody string `json:"textbody"`
+}
+
+// Return new API client
+func NewClient(apiKey string) *Client {
+	return &Client{
+		apiKey: apiKey,
+	}
+}
+
+// Send an email through the API
+func (c *Client) Send(payload *Payload) error {
+	url := fmt.Sprintf("%s/%s", API_URL, "send")
+
+	// Marshal the payload into JSON format
+	payloadJSON, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	// Create and configure HTTP request
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payloadJSON))
+	if err != nil {
+		return err
+	}
+
+	// Add appropriate headers
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("OhMySMTP-Server-Token", c.apiKey)
+
+	// Execute the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
